3b683c400c1669cc0bafaccd61517f1a6c9cdb70
고의동 <kouydong@aegisep.com>
2022-07-03 07:59:40 +0900

    DB다중연결예제

--- src/main/java/com/example/multipleDbConnection/MultipleDbConnectionApplication.java
+++ src/main/java/com/example/multipleDbConnection/MultipleDbConnectionApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Dbtest2Application {
+public class MultipleDbConnectionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Dbtest2Application.class, args);
+		SpringApplication.run(MultipleDbConnectionApplication.class, args);
 	}
 
 }
